32d61009f2b5ac159e2bba8277a7bd7ed8a85197
shang6723 <31940209+shang6723@users.noreply.github.com>
2017-10-19 23:53:21 -0700

    v1.0.0
    draft

---
+++ js/map.js
@@ -0,0 +1,7 @@
+    var map;
+    function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: 34.021, lng: -118.286},
+            zoom: 16
+        });
+    }
