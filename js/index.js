cd98b34eb1a5bef7a16efb5c4823ad2f104a1e9c
shang6723 <31940209+shang6723@users.noreply.github.com>
2017-10-25 23:46:23 -0700

    v1.0.4
    add submenu

--- js/index.js
+++ js/index.js
@@ -1,18 +1,15 @@
     function myFunction() {
-        document.getElementById("myDropdown").classList.toggle("show");
+        document.getElementById("MenuDropdown").classList.toggle("show");
     }
     
-    // Close the dropdown menu if the user clicks outside of it
-    //window.onclick = function(event) {
-    //  if (!event.target.matches('.dropbtn')) {
-    //
-    //    var dropdowns = document.getElementsByClassName("dropdown-content");
-    //    var i;
-    //    for (i = 0; i < dropdowns.length; i++) {
-    //      var openDropdown = dropdowns[i];
-    //      if (openDropdown.classList.contains('show')) {
-    //        openDropdown.classList.remove('show');
-    //      }
-    //    }
-    //  }
-    //};
+    function FSRDropdown_fun() {
+        var tag = document.getElementById("FSRDropdown");
+        if (tag.style.display === "block") tag.style.display = "none";
+        else if (tag.style.display === "none" || tag.style.display === "") tag.style.display = "block";
+    }
+    
+    function QSRDropdown_fun() {
+        var tag = document.getElementById("QSRDropdown");
+        if (tag.style.display === "block") tag.style.display = "none";
+        else if (tag.style.display === "none" || tag.style.display === "") tag.style.display = "block";
+    }
