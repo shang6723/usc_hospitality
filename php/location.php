c84235959306eacd03b924b505462b705f955405
shang6723 <31940209+shang6723@users.noreply.github.com>
2017-11-10 19:44:41 -0800

    v1.0.5
    Add db connect

---
+++ php/location.php
@@ -0,0 +1,47 @@
+<?php
+require("db_info.php");
+error_reporting(E_ALL);
+ini_set("display_errors",1);
+
+$dom = new DOMDocument("1.0");
+$node = $dom->createElement("markers");
+$parnode = $dom->appendChild($node);
+
+// Opens a connection to a MySQL server
+
+$connection=mysqli_connect ('mysql.cs.ccu.edu.tw', $username, $password, $database);
+if (!$connection) {  die('Not connected : ' . mysqli_error($connection));}
+
+// Set the active MySQL database
+
+$db_selected = mysqli_select_db($connection, $database);
+if (!$db_selected) {
+  die ('Can\'t use db : ' . mysqli_error($connection));
+}
+
+// Select all the rows in the markers table
+
+$query = "SELECT * FROM uscfood_location WHERE 1";
+$result = mysqli_query($connection, $query);
+if (!$result) {
+  die('Invalid query: ' . mysqli_error($connection));
+}
+
+header("Content-type: text/xml");
+
+// Iterate through the rows, adding XML nodes for each
+
+while ($row = @mysqli_fetch_array($result)){
+  // Add to XML document node
+  $node = $dom->createElement("marker");
+  $newnode = $parnode->appendChild($node);
+  $newnode->setAttribute("name",$row['name']);
+  $newnode->setAttribute("address", $row['address']);
+  $newnode->setAttribute("lat", $row['lat']);
+  $newnode->setAttribute("lng", $row['lng']);
+  $newnode->setAttribute("type", $row['type']);
+}
+
+echo $dom->saveXML();
+
+?>
